1c0c6caf58bf7828afda4cecbc8c29fa39d0a9b0
Adam Roberts <adamroberts@cshs-MBP.lan>
2024-09-15 10:55:49 -0700

    feat: getter function for tags after initialization

--- src/TagManager.js
+++ src/TagManager.js
@@ -1,11 +1,14 @@
 import Snippets from './Snippets'
 
 const TagManager = {
+  createdTags: {},
+
   dataScript: function (dataLayer) {
     const script = document.createElement('script')
     script.innerHTML = dataLayer
     return script
   },
+
   gtm: function (args) {
     const snippets = Snippets.tags(args)
 
@@ -29,6 +32,7 @@ const TagManager = {
       dataScript
     }
   },
+
   initialize: function ({ gtmId, events = {}, dataLayer, dataLayerName = 'dataLayer', auth = '', preview = '' }) {
     const gtm = this.gtm({
       id: gtmId,
@@ -38,15 +42,34 @@ const TagManager = {
       auth,
       preview
     })
-    if (dataLayer) document.head.appendChild(gtm.dataScript)
-    document.head.insertBefore(gtm.script(), document.head.childNodes[0])
-    document.body.insertBefore(gtm.noScript(), document.body.childNodes[0])
+
+    if (dataLayer) {
+      const dataScriptElement = gtm.dataScript
+      document.head.appendChild(dataScriptElement)
+      this.createdTags.dataScript = dataScriptElement
+    }
+
+    const scriptElement = gtm.script()
+    document.head.insertBefore(scriptElement, document.head.childNodes[0])
+    this.createdTags.script = scriptElement
+
+    const noScriptElement = gtm.noScript()
+    document.body.insertBefore(noScriptElement, document.body.childNodes[0])
+    this.createdTags.noScript = noScriptElement
   },
-  dataLayer: function ({dataLayer, dataLayerName = 'dataLayer'}) {
-    if (window[dataLayerName]) return window[dataLayerName].push(dataLayer)
+
+  dataLayer: function ({ dataLayer, dataLayerName = 'dataLayer' }) {
+    if (window[dataLayerName]) {
+      return window[dataLayerName].push(dataLayer)
+    }
+
     const snippets = Snippets.dataLayer(dataLayer, dataLayerName)
     const dataScript = this.dataScript(snippets)
     document.head.insertBefore(dataScript, document.head.childNodes[0])
+  },
+
+  getCreatedTags: function () {
+    return this.createdTags
   }
 }
 
